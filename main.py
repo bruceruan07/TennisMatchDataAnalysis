98737ff551bfbfd17e78ccc31f53fcb62530b087
Bruce <bruce.h.ruan@gmail.com>
2024-03-30 12:00:03 -0700

    Ensured player names are consistent

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ df = pd.read_csv('atp_data.csv')
 
 
 df.dropna(subset=['Wsets', 'Lsets', 'PSW', 'PSL', 'B365W', 'B365L'], inplace=True)
+df['Winner'] = df['Winner'].str.lower().str.strip()
+df['Loser'] = df['Loser'].str.lower().str.strip()
 
-
-print(df.isnull().sum())
+print(df['Winner'])
