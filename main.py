67aa3ea8d87a1c983ba9397f1deb6a50df76f2e2
Bruce <bruce.h.ruan@gmail.com>
2024-03-30 11:55:19 -0700

    Dropped rows with missing values

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('atp_data.csv')
+
+
+print(df.isnull().sum())
+df.dropna(subset=['Wsets', 'Lsets', 'PSW', 'PSL', 'B365W', 'B365L'])
